07ccbede97a017c274b5199a145c02ac7adf162e
Jake Walter <jakewalter080@gmail.com>
2024-09-22 19:43:05 -0500

    add role table schema

--- sql_commands/schema.sql
+++ sql_commands/schema.sql
@@ -3,3 +3,10 @@ CREATE TABLE department (
     name VARCHAR(30) UNIQUE NOT NULL
 );
 
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department (id)
+);
