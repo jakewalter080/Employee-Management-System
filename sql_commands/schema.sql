cb39bfe897e724fbb6bfdf2310243031c1d565ca
Jake Walter <jakewalter080@gmail.com>
2024-09-22 19:43:47 -0500

    add table for employee schema

--- sql_commands/schema.sql
+++ sql_commands/schema.sql
@@ -10,3 +10,13 @@ CREATE TABLE role (
     department_id INTEGER NOT NULL,
     FOREIGN KEY (department_id) REFERENCES department (id)
 );
+
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES role (id),
+    FOREIGN KEY (manager_id) REFERENCES employee (id)
+);
