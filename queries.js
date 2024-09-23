71f2d69588c76c308903f4e93ada04d02c58a717
Jake Walter <jakewalter080@gmail.com>
2024-09-22 19:35:07 -0500

    add queries for employee tracker

--- queries.js
+++ queries.js
@@ -13,23 +13,59 @@ module.exports = {
     `,
 
     VIEW_ALL_EMPLOYEES; `
-    SELECT 
-    employee.id, 
-    employee.first_name, 
-    employee.last_name, 
-    role.title, 
-    department.name AS department, 
-    role.salary, 
-    CONCAT(manager.first_name, ' ', manager.last_name) AS manager
-    FROM employee e
-    JOIN role r ON e.role_id = r.id
-    JOIN department d ON r.department_id = d.id
-    LEFT JOIN employee m ON e.manager_id = m.id
-    ORDER BY employee.id;
+        SELECT 
+        employee.id, 
+        employee.first_name, 
+        employee.last_name, 
+        role.title, 
+        department.name AS department, 
+        role.salary, 
+        CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+        FROM employee e
+        JOIN role r ON e.role_id = r.id
+        JOIN department d ON r.department_id = d.id
+        LEFT JOIN employee m ON e.manager_id = m.id
+        ORDER BY employee.id;
     `,
 
     ADD_DEPARTMENT; `
-    INSERT INTO department (name)
-    VALUES ($1)
-    RETURNING id;
+        INSERT INTO department (name)
+        VALUES ($1)
+        RETURNING id;
     `,
+
+    ADD_ROLE; `
+        INSERT INTO role (title, salary, department_id)
+        VALUES ($1, $2, $3)
+        RETURNING id;
+    `,
+
+    ADD_EMPLOYEE; `
+        INSERT INTO employee (first_name, last_name, role_id, manager_id)
+        VALUES ($1, $2, $3, $4)
+        RETURNING id;
+    `,
+
+    UPDATE_EMPLOYEE_ROLE; `
+        UPDATE employee
+        SET role_id = $1
+        WHERE id = $2;
+    `,
+
+    GET_ALL_DEPARTMENTS; `
+        SELECT id, name
+        FROM department;
+        ORDER BY name;
+    `,
+
+    GET_ALL_ROLES; `
+        SELECT id, title
+        FROM role;
+        ORDER BY title;
+    `,
+    GET_ALL_EMPLOYEES; `
+        SELECT id, CONCAT(first_name, ' ', last_name) AS name
+        FROM employee
+        ORDER BY last_name, first_name;
+    `,
+};
