90c5c68c3eae068a06da1491a5b0161892e46ce0
Jake Walter <jakewalter080@gmail.com>
2024-09-23 21:28:13 -0500

    fixed VIEW_ALL_ROLES and VIEW_ALL_EMPLOYEES table mixing

--- queries.js
+++ queries.js
@@ -6,27 +6,27 @@ module.exports = {
     `,
 
     VIEW_ALL_ROLES: `
-        SELECT role.id, role.title, role.salary, department.name AS department
-        FROM role r
-        JOIN department d ON r.department_id = d.id
-        ORDER BY role.id;
-    `,
+    SELECT r.id, r.title, r.salary, d.name AS department
+    FROM role r
+    JOIN department d ON r.department_id = d.id
+    ORDER BY r.id;
+`,
 
-    VIEW_ALL_EMPLOYEES: `
-        SELECT 
-        employee.id, 
-        employee.first_name, 
-        employee.last_name, 
-        role.title, 
-        department.name AS department, 
-        role.salary, 
-        CONCAT(manager.first_name, ' ', manager.last_name) AS manager
-        FROM employee e
-        JOIN role r ON e.role_id = r.id
-        JOIN department d ON r.department_id = d.id
-        LEFT JOIN employee m ON e.manager_id = m.id
-        ORDER BY employee.id;
-    `,
+VIEW_ALL_EMPLOYEES: `
+    SELECT 
+    employee.id, 
+    employee.first_name, 
+    employee.last_name, 
+    role.title, 
+    department.name AS department, 
+    role.salary, 
+    CONCAT(mmanager.first_name, ' ', mmanager.last_name) AS manager
+    FROM employee e
+    JOIN role r ON e.role_id = r.id
+    JOIN department d ON r.department_id = d.id
+    LEFT JOIN employee m ON e.manager_id = m.id
+    ORDER BY e.id;
+`,
 
     ADD_DEPARTMENT: `
         INSERT INTO department (name)
